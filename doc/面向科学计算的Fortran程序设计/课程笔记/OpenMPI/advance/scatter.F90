ff31729eaed8fccee9081acbf92b7c67d801c06f
chillcicada <2210227279@qq.com>
2023-12-18 21:53:17 +0800

    add: Fortran HW9

---
+++ doc/面向科学计算的Fortran程序设计/课程笔记/OpenMPI/advance/scatter.F90
@@ -0,0 +1,19 @@
+program scatter
+  use mpi
+  integer isend(4), recv, rank, size
+
+  call mpi_init(ierr)
+  call mpi_comm_rank(MPI_COMM_WORLD, rank, ierr)
+  call mpi_comm_size(MPI_COMM_WORLD, size, ierr)
+
+  if(rank .eq. 0) then
+    do i = 1, size
+      isend(i) = i
+    enddo
+  endif
+
+  call mpi_scatter(isend, 1, MPI_INTEGER,irecv, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+
+  print *, 'rank = ', rank , ': ','irecv= ', irecv
+  call mpi_finalize(ierr)
+end program
