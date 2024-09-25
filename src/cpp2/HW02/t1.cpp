4d2eed14c4f7cfe2e2c54bba0a23693b72a82aaa
chillcicada <2210227279@qq.com>
2024-09-25 22:16:22 +0800

    feat: cpp2/hw02

--- src/cpp2/HW02/t1.cpp
+++ src/cpp2/HW02/t1.cpp
@@ -2,36 +2,32 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+inline bool isDigit(char c) { return c >= '0' && c <= '9'; }
 
 int main() {
-  for (string s; getline(cin, s);) { // 遍历输入的每一行
-    bool first;                      // 是否是当前行的第一个整数
-    size_t n = s.size();
-    for (size_t i = 0; i < n; i++)
-      if (s[i] >= 0 && s[i] <= 9) {
-        // s[i] 是一个数字
-        // 下面寻找以 s[i] 开始的连续一段数字的结尾在哪里
-        size_t r = i + 1;
-        while (s[r] >= '0' && s[r] < '9')
-          ++r;
-        // 到目前为止，找到了以 s[i] 开始，s[r] 结束的一段数字
-        // 此时 s[r] 是数字，s[r + 1] 不是数字
+  // iterate over each line of input
+  for (std::string str; getline(std::cin, str);) {
+    // flag: is the first integer of the current line, default to true
+    bool isFirstInt{true};
+    size_t strLength = str.size();
 
-        // 如果不是当前行的第一个整数，则先输出一个空格
-        if (first)
-          first = false;
+    for (size_t i = 0; i < strLength; i++)
+      if (isDigit(str[i])) { // str[i] is a digit
+        size_t r = i + 1;
+        while (isDigit(str[r]))
+          ++r; // now str[r - 1] is a digit, str[r] is not a digit
+
+        // use the flag
+        if (isFirstInt)
+          isFirstInt = false;
         else
           putchar(' ');
 
-        // 输出当前整数
-        for (size_t j = i; j <= r; j++)
-          putchar(s[j]);
+        for (size_t j = i; j < r; j++)
+          putchar(str[j]);
 
-        // 跳过当前已输出完毕的整数
-        // 这样在输出 1234 以后不会再输出 234 和 34，而是从 4 后面寻找下一个数
-        i = r;
-      }
+        i = r; // skip the current integer
+      };
     putchar('\n');
   }
   return 0;
