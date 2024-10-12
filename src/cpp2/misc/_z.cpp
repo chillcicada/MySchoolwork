764217eb34a738651aa579dc3b7744f9ecffac76
chillcicada <2210227279@qq.com>
2024-10-13 01:02:41 +0800

    fix: update report2

---
+++ src/cpp2/misc/_z.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct Base {
+  virtual ~Base() = default; // virtual ~Base() {}
+};
+
+struct Derived : Base {
+private:
+  char *p;
+
+public:
+  Derived(unsigned size) {
+    p = new char[size];
+    std::cout << "ctor called" << std::endl;
+  }
+  ~Derived() {
+    delete[] p;
+    std::cout << "dtor called" << std::endl;
+  }
+};
+
+int main() {
+  Base *b{nullptr};
+  b = new Derived(16);
+  // [...]
+  delete b;
+}
