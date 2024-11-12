55572c9fde7c697610073025f5280a7c81e38a69
chillcicada <2210227279@qq.com>
2024-11-12 22:23:03 +0800

    feat: update my stl

---
+++ src/cpp2/tests/my_list_test.cpp
@@ -0,0 +1,122 @@
+#include "../mystl/my_list.h"
+#include <utest.h>
+
+UTEST(my_list, base) {
+  MyList<int> l;
+  ASSERT_TRUE(l.empty());
+  ASSERT_EQ(0, l.size());
+}
+
+UTEST(my_list, copy_constructor) {
+  MyList<int> l;
+  l.append(1);
+  l.append(2);
+  l.append(3);
+  MyList<int> l2 = l;
+  ASSERT_EQ(3, l2.size());
+  ASSERT_EQ(1, l2.head->data);
+  ASSERT_EQ(3, l2.tail->data);
+}
+
+UTEST(my_list, move_constructor) {
+  MyList<int> l;
+  l.append(1);
+  l.append(2);
+  l.append(3);
+  MyList<int> l2 = std::move(l);
+  ASSERT_EQ(3, l2.size());
+  ASSERT_EQ(1, l2.head->data);
+  ASSERT_EQ(3, l2.tail->data);
+  ASSERT_TRUE(l.empty());
+}
+
+UTEST(my_list, copy_assignment) {
+  MyList<int> l;
+  l.append(1);
+  l.append(2);
+  l.append(3);
+  MyList<int> l2;
+  l2 = l;
+  ASSERT_EQ(3, l2.size());
+  ASSERT_EQ(1, l2.head->data);
+  ASSERT_EQ(3, l2.tail->data);
+}
+
+UTEST(my_list, move_assignment) {
+  MyList<int> l;
+  l.append(1);
+  l.append(2);
+  l.append(3);
+  MyList<int> l2;
+  l2 = std::move(l);
+  ASSERT_EQ(3, l2.size());
+  ASSERT_EQ(1, l2.head->data);
+  ASSERT_EQ(3, l2.tail->data);
+  ASSERT_TRUE(l.empty());
+}
+
+UTEST(my_list, operator_brackets) {
+  MyList<int> l;
+  l.append(1);
+  l.append(2);
+  l.append(3);
+  ASSERT_EQ(1, l[0]);
+  ASSERT_EQ(2, l[1]);
+  ASSERT_EQ(3, l[2]);
+}
+
+UTEST(my_list, append_remove) {
+  MyList<int> l;
+  l.append(1); // 1
+  l.append(2); // 1 2
+  l.append(3); // 1 2 3
+  ASSERT_EQ(3, l.size());
+
+  l.remove(2); // 1 3
+  ASSERT_EQ(2, l.size());
+  ASSERT_EQ(1, l.head->data);
+  ASSERT_EQ(3, l.tail->data);
+
+  l.remove(1); // 3
+  ASSERT_EQ(1, l.size());
+  ASSERT_EQ(3, l.head->data);
+  ASSERT_EQ(3, l.tail->data);
+
+  l.remove(3);
+  ASSERT_EQ(0, l.size());
+  ASSERT_TRUE(l.empty());
+}
+
+UTEST(my_list, append_at) {
+  MyList<int> l;
+  l.append(1);      // 1
+  l.append(2);      // 1 2
+  l.append(3);      // 1 2 3
+  l.appendAt(1, 4); // 1 4 2 3
+  ASSERT_EQ(4, l.size());
+  ASSERT_EQ(1, l.head->data);
+  ASSERT_EQ(3, l.tail->data);
+  ASSERT_EQ(4, l.head->next->data);
+  ASSERT_EQ(2, l.head->next->next->data);
+}
+
+UTEST(my_list, remove_at) {
+  MyList<int> l;
+  l.append(1);   // 1
+  l.append(2);   // 1 2
+  l.append(3);   // 1 2 3
+  l.removeAt(1); // 1 3
+  ASSERT_EQ(2, l.size());
+  ASSERT_EQ(1, l.head->data);
+  ASSERT_EQ(3, l.tail->data);
+}
+
+UTEST(my_list, clear) {
+  MyList<int> l;
+  l.append(1);
+  l.append(2);
+  l.append(3);
+  l.clear();
+  ASSERT_TRUE(l.empty());
+  ASSERT_EQ(0, l.size());
+}
---
+++ src/cpp2/tests/my_queue_test.cpp
@@ -0,0 +1,67 @@
+#include "../mystl/my_queue.h"
+#include <utest.h>
+
+UTEST(my_queue, default_constructor) {
+  MyQueue<int> my_queue;
+  ASSERT_TRUE(my_queue.empty());
+  ASSERT_EQ(0, my_queue.size());
+}
+
+UTEST(my_queue, push) {
+  MyQueue<int> my_queue;
+  my_queue.push(1);
+  my_queue.push(2);
+  my_queue.push(3);
+  ASSERT_FALSE(my_queue.empty());
+  ASSERT_EQ(3, my_queue.size());
+}
+
+UTEST(my_queue, pop) {
+  MyQueue<int> my_queue;
+  my_queue.push(1);
+  my_queue.push(2);
+  my_queue.push(3);
+  my_queue.pop();
+  ASSERT_EQ(2, my_queue.size());
+  my_queue.pop();
+  ASSERT_EQ(1, my_queue.size());
+  my_queue.pop();
+  ASSERT_TRUE(my_queue.empty());
+}
+
+UTEST(my_queue, front) {
+  MyQueue<int> my_queue;
+  my_queue.push(1);
+  my_queue.push(2);
+  my_queue.push(3);
+  ASSERT_EQ(1, my_queue.front());
+  my_queue.pop();
+  ASSERT_EQ(1, my_queue.front());
+  my_queue.pop();
+  ASSERT_EQ(1, my_queue.front());
+}
+
+UTEST(my_queue, back) {
+  MyQueue<int> my_queue;
+  my_queue.push(1);
+  my_queue.push(2);
+  my_queue.push(3);
+  ASSERT_EQ(3, my_queue.back());
+  my_queue.pop();
+  ASSERT_EQ(2, my_queue.back());
+  my_queue.pop();
+  ASSERT_EQ(1, my_queue.back());
+}
+
+UTEST(my_queue, size) {
+  MyQueue<int> my_queue;
+  ASSERT_EQ(0, my_queue.size());
+  my_queue.push(1);
+  ASSERT_EQ(1, my_queue.size());
+  my_queue.push(2);
+  ASSERT_EQ(2, my_queue.size());
+  my_queue.pop();
+  ASSERT_EQ(1, my_queue.size());
+  my_queue.pop();
+  ASSERT_EQ(0, my_queue.size());
+}
